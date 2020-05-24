969793683b7305f531487b2d64ad1dc84762805f
Ruan Pearce-Authers <ruanpa@outlook.com>
2020-05-24 22:39:43 +0100

    Save table value in data test bootstrap

--- tests/data.rs
+++ tests/data.rs
@@ -7,6 +7,7 @@ use data::*
 conn <- connect_sqlite(":memory:")
 execute(conn, "create table test_table (name varchar(255), age integer)")
 execute(conn, "insert into test_table (name, age) values ('ruan', 26), ('ruanlater', 27), ('thirdperson', 27), ('ancient one', null)")
+test_table <- table(conn, "test_table")
 "#;
 
 fn with_table_bootstrap(query: &str) -> String {
@@ -17,19 +18,17 @@ fn with_table_bootstrap(query: &str) -> String {
 fn test_data_sqlite() {
 	helpers::eval_expect_values(&[
 		(
-			&with_table_bootstrap(r#"num_rows(collect(table(conn, "test_table")))"#),
+			&with_table_bootstrap(r#"num_rows(collect(test_table))"#),
 			Value::Int(4),
 		),
 		(
-			&with_table_bootstrap(
-				r#"num_rows(collect(filter(table(conn, "test_table"), name == "ruan")))"#,
-			),
+			&with_table_bootstrap(r#"num_rows(collect(filter(test_table, name == "ruan")))"#),
 			Value::Int(1),
 		),
 		(
 			&with_table_bootstrap(
 				r#"name_to_find <- "ancient one"
-				num_rows(collect(filter(table(conn, "test_table"), name == {{name_to_find}})))"#,
+				num_rows(collect(filter(test_table, name == {{name_to_find}})))"#,
 			),
 			Value::Int(1),
 		),
